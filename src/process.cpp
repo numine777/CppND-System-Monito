a57e4fffa66356a331b44e4dc015ced39ddecda0
Scott Walls <61322440+numine777@users.noreply.github.com>
2020-06-13 09:45:40 -0500

    Update process.cpp

--- src/process.cpp
+++ src/process.cpp
@@ -21,16 +21,11 @@ int Process::Pid() { return pid; }
 
 // TODO: Return this process's CPU utilization
 float Process::CpuUtilization() { 
-    vector<long> data = LinuxParser::CpuUtilization(this->pid);
-    float utime = (float)data[0];
-    float stime = (float)data[1];
-    float cutime = (float)data[2];
-    float cstime = (float)data[3];
-    starttime = (float)data[4];
-    float total_time = utime + stime + cutime + cstime;
-    float seconds = UpTime() - (starttime / (float) sysconf(_SC_CLK_TCK));
-
-    return (total_time/(float)sysconf(_SC_CLK_TCK))/seconds; 
+    vector<long> data =LinuxParser::pidjiffys(this->pid);
+    long total_time = data[kutime_] + data[kstime_] + data[kcstime_] + data[kcutime_];
+    long sec = LinuxParser::UpTime() - (data[kstarttime_]/sysconf(_SC_CLK_TCK)) ;
+    float cpu_usage = ((float)total_time /(float) sysconf(_SC_CLK_TCK))/(float)sec;
+    return cpu_usage; 
 }
 
 // TODO: Return the command that generated this process
@@ -43,7 +38,7 @@ string Process::Ram() { return LinuxParser::Ram(pid); }
 string Process::User() { return LinuxParser::User(pid); }
 
 // TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return starttime / sysconf(_SC_CLK_TCK); }
+long int Process::UpTime() { LinuxParser::UpTime(pid); }
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
